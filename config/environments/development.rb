bbae52ee63673d64fde0294fdbe699dc1dc7689a
gnvvbll <gnvvbll@gmail.com>
2019-09-20 13:40:27 -0400

    made alert boxes and defined conditions for them to appear. Also added new user functionality

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.action_mailer.default_url_options = { host: 'localhost:3000'}
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
@@ -60,3 +61,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
+
+
