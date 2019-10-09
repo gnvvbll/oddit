4bad7d500079ae3b2dddb08fd776c9f083873d7d
gnvvbll <gnvvbll@gmail.com>
2019-10-09 11:54:51 -0400

    Troubleshooting Lesson 41 emails not sending

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -2,7 +2,7 @@ class NotificationMailer < ApplicationMailer
     default from: "no-reply@oddit.com"
 
     def comment_added
-        mail(to: "gnvvbll@gmail.com",
+        mail(to: "genevieve.bell.code@gmail.com",
             subject: "A comment has been added to your place")
     end
 end
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,9 +3,9 @@ CarrierWave.configure do |config|
     config.fog_public = false
     config.fog_credentials = {
       provider:              'AWS',                        # required
-      aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-      aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+      aws_access_key_id:     "AKIAQWHUZWS654EORPEY",        # required
+      aws_secret_access_key: "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",        # required
       region: 'us-east-2'
     }
-    config.fog_directory  = ENV["AWS_BUCKET"]              # required
-  end
+    config.fog_directory  = "/uploads"              # required
+  end 
