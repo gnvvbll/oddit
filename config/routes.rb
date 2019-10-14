eecb4c97fe533b043218707401989c23cec0669f
gnvvbll <gnvvbll@gmail.com>
2019-10-14 16:45:50 -0400

    adding user info to profile page

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,9 @@ Rails.application.routes.draw do
   resources :places do
     resources :comments, only: :create
     resources :photos, only: :create
-    resources :users, only: :show
    end 
 
-
+   resources :users, only: :show
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
