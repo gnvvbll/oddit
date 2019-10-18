9765b46c9a097b53af6a0b3c297018e5ad3f94d4
gnvvbll <gnvvbll@gmail.com>
2019-10-17 21:54:32 -0400

    final test

--- db/migrate/20191004193838_add_user_ref_to_photos.rb
+++ db/migrate/20191004193838_add_user_ref_to_photos.rb
@@ -1,7 +1,5 @@
 class AddUserRefToPhotos < ActiveRecord::Migration[6.0]
   def change
-    has_many :photos
-    belongs_to :place
     add_reference :photos, :user, null: false, foreign_key: true
   end
 end
