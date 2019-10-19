c519027c3019290bb5fe72be6e10f15dc6689312
gnvvbll <gnvvbll@gmail.com>
2019-10-19 14:16:40 -0400

    troubleshooting create_photos.rb

--- db/migrate/20191001212109_add_picture_to_photos.rb
+++
@@ -1,7 +0,0 @@
-class AddPictureToPhoto < ActiveRecord::Migration[6.0]
-  def change
-    add_column :photos, :picture, :string
-  end
-end
-
-
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_04_193838) do
+ActiveRecord::Schema.define(version: 2019_09_30_164555) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,14 +27,14 @@ ActiveRecord::Schema.define(version: 2019_10_04_193838) do
   end
 
   create_table "photos", force: :cascade do |t|
-    t.bigint "place_id"
+    t.text "caption"
+    t.string "picture"
+    t.integer "place_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "picture"
-    t.text "caption"
-    t.bigint "user_id", null: false
     t.index ["place_id"], name: "index_photos_on_place_id"
-    t.index ["user_id"], name: "index_photos_on_user_id"
+    t.index ["user_id", "place_id"], name: "index_photos_on_user_id_and_place_id"
   end
 
   create_table "places", force: :cascade do |t|
@@ -61,5 +61,4 @@ ActiveRecord::Schema.define(version: 2019_10_04_193838) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key "photos", "users"
 end
