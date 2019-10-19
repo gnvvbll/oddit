fdc0fa9d8af1b37bd5b073999fadeb63a55bb1d8
gnvvbll <gnvvbll@gmail.com>
2019-10-19 13:16:53 -0400

    troubleshooting issues with pushing to heroku

--- db/migrate/20190930164555_create_photos.rb
+++ db/migrate/20190930164555_create_photos.rb
@@ -1,8 +1,13 @@
 class CreatePhotos < ActiveRecord::Migration[6.0]
   def change
     create_table :photos do |t|
-      t.references :place
+      t.text :caption
+        t.integer :place_id
+        t.integer :user_id
       t.timestamps
     end
+
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
   end
 end
--- db/migrate/20191002174101_add_caption_to_photo.rb
+++
@@ -1,5 +0,0 @@
-class AddCaptionToPhoto < ActiveRecord::Migration[6.0]
-  def change
-    add_column :photos, :caption, :text
-  end
-end
